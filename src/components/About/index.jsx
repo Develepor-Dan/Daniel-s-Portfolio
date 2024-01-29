29267da2943eddb4dcd38f6357ea1c4685eca74b
Daniel Zavala <zavaladaniel151@gmail.com>
2024-01-29 02:03:53 -0500

    Update About jsx

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import avatarImage from '../../assets/avatar/Avatar.png';
 
 function AboutMe() {
   return (
     <section className="about-me-container my-5">
-      <div class="uk-height-medium uk-flex uk-flex-center uk-flex-middle uk-background-cover uk-light" data-src="../assets/avatar/Avatar.png" uk-img>
-        <img src="../src/assets/avatar/Avatar.png" alt="" />
-      </div>      
-      <div className="about-text-container">
+      <figure className=" py-2 uk-height-medium uk-flex uk-flex-center uk-flex-middle uk-background-cover uk-light"  uk-img={true.toString()}>
+      <img src={avatarImage} alt="Daniel Zavala's profile" />
+      </figure>
+      <div className="about-text-container py-2">
         <p>
           Hi there! I'm Daniel Zavala, a passionate developer with a love for building
           innovative web applications.
