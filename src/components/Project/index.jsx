a2342ecb3c5a3be26895ef4d90abda687b395701
Daniel Zavala <zavaladaniel151@gmail.com>
2024-01-29 01:14:31 -0500

    Create Project jsx

---
+++ src/components/Project/index.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { removeHyphensAndCapitalize } from '../../utils/helpers';
+import * as projects from '../../assets';
+
+const Project = ({ name, description, link, repo }) => {
+
+  const formattedName = removeHyphensAndCapitalize(name);
+
+  return (
+    <div className="project" key={name}>
+      <img
+        src={projects[name]}
+        alt={formattedName}
+        className="project-bg"
+      />
+      <div className="project-text">
+        <h3>
+          <a href={link}>{formattedName}</a>
+          &nbsp;
+          <a href={repo}>
+            <i className="fab fa-github" />
+          </a>
+        </h3>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Project;
