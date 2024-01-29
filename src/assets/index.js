c4680041c625c622645a24b2392211d372a3d9b7
Daniel Zavala <zavaladaniel151@gmail.com>
2024-01-29 03:01:29 -0500

    Update portfolio export js

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,6 @@
-export { default as calculator } from './projects/calculator.jpg';
-export { default as ledWall } from './projects/ledWall.jpg';
-export { default as pastelPuzzels } from './projects/pastelPuzzels.jpg';
-export { default as runBuddy } from './projects/runBuddy.jpg';
-export { default as surfReport } from './projects/surfReport.jpg';
+export { default as TechTrailblazer } from './projects/TechTrailblazer.jpg';
+export { default as NoteTakerExpress } from './projects/NoteTakerExpress.gif';
+export { default as GoalMinder } from './projects/GoalMinder.jpg';
+export { default as DepartmentIdeas } from './projects/DepartmentIdeas.jpg';
+export { default as READMEGenerator } from './projects/READMEGenerator.gif';
+export { default as MuttMemes } from './projects/MuttMemes.jpg';
