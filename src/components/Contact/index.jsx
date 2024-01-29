dca2ce739ef09afda137c898f394251a1e69a050
Daniel Zavala <zavaladaniel151@gmail.com>
2024-01-29 01:15:05 -0500

    Create Contact jsx

---
+++ src/components/Contact/index.jsx
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from 'react';
+import { validateEmail } from '../../utils/helpers';
+
+function Contact() {
+  const [formState, setFormState] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  const [errorMessage, setErrorMessage] = useState('');
+  const { name, email, message } = formState;
+
+  useEffect(() => {
+    console.log('Form State:', formState);
+  }, [formState]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!errorMessage) {
+      console.log('Submit Form', formState);
+    }
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    // Validate email
+    if (name === 'email') {
+      const isValid = validateEmail(value);
+      setErrorMessage(isValid ? '' : 'Your email is invalid.');
+    } else {
+      // Check for other fields
+      setErrorMessage(value.length ? '' : `${name} is required.`);
+    }
+
+    // Update form state
+    if (!errorMessage) {
+      setFormState({ ...formState, [name]: value });
+    }
+  };
+
+  return (
+    <section>
+      <form id="contact-form" onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input type="text" name="name" defaultValue={name} onBlur={handleChange} />
+        </div>
+        <div>
+          <label htmlFor="email">Email address:</label>
+          <input type="email" name="email" defaultValue={email} onBlur={handleChange} />
+        </div>
+        <div>
+          <label htmlFor="message">Message:</label>
+          <textarea name="message" rows="5" defaultValue={message} onBlur={handleChange} />
+        </div>
+        {errorMessage && <p className="error-text">{errorMessage}</p>}
+        <button type="submit">Submit</button>
+      </form>
+    </section>
+  );
+}
+
+export default Contact;
