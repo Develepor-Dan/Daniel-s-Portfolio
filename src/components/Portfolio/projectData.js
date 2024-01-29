02ff674a70f66c082a943ee5e509225cda4e1b53
Daniel Zavala <zavaladaniel151@gmail.com>
2024-01-29 01:16:22 -0500

    Create projectData js
    project data to go with portfolio

---
+++ src/components/Portfolio/projectData.js
@@ -0,0 +1,49 @@
+// projectData.js
+const projectData = [
+    {
+      id: '1',
+      name: 'surfReport',
+      description: 'MERN Stack',
+      link: "https://github.com",
+      repo: "https://github.com"
+    },
+    {
+      id: '2',
+      name: 'pastelPuzzels',
+      description: 'MERN Stack',
+      link: "https://github.com",
+      repo: "https://github.com"
+    },
+    {
+      id: '3',
+      name: 'runBuddy',
+      description: 'HTML/CSS',
+      link: "https://github.com",
+      repo: "https://github.com"
+    },
+    {
+      id: '4',
+      name: 'ledWall',
+      description: 'Node/IoT',
+      link: "https://github.com",
+      repo: "https://github.com"
+    },
+    {
+      id: '5',
+      name: 'calculator',
+      description: 'React/JavaScript/CSS',
+      link: "https://github.com",
+      repo: "https://github.com"
+    },
+    {
+      id: '6',
+     name: 'calculator',
+     description: 'React/JavaScript/CSS',
+     link: "https://github.com",
+     repo: "https://github.com"
+   },
+
+   ];
+   
+   export default projectData;
+   
